0342803e4d4fb51c53aab376e205e5abcdb93340
Tim Green <tiimgreen@gmail.com>
2014-08-03 22:03:18 +0100

    .js.coffee -> .js, update shanizzle

--- app/controllers/crawls_controller.rb
+++ app/controllers/crawls_controller.rb
@@ -1,6 +1,6 @@
 class CrawlsController < ApplicationController
   def new
-    #DelayedJob::enqueue CrawlJob.new(params[:site_id])
+    #Delayed::Job.enqueue CrawlJob.new(params[:site_id])
     CrawlJob.new(params[:site_id]).perform
     flash[:success] = 'Crawling site'
     redirect_to site_path params[:site_id]
--- lib/crawl_job.rb
+++ lib/crawl_job.rb
@@ -2,28 +2,48 @@ class CrawlJob < Struct.new(:site_id)
   def perform
     require 'open-uri'
     @site = Site.find(site_id)
+    @text_to_search = ["Lorem ipsum", "lorem ipsum", "Lorem Ipsum", "LOREM IPSUM"]
+    @links = []
+
     @site.update_attributes(currently_crawling: true, last_crawled: Time.now)
-    @links = [@site.url]
 
     @site.crawling_errors.each { |e| e.destroy }
 
     crawl_page @site.url
     @site.update_attributes(currently_crawling: false)
   end
-  handle_asynchronously :perform
 
   def crawl_page(url, options = {})
+    search_for_lorem url
+    gather_links_on_page url
+  end
+
+  def search_for_lorem(url)
     doc = Nokogiri::HTML open(url)
-    lorem_results = doc.search "[text()*='digitally']"
-    if lorem_results
+    lorem_results = []
+
+    for string in @text_to_search
+      results = doc.search("[text()*='#{string}']")
+      lorem_results.push(results) if results.present?
+    end
+
+    if lorem_results.any?
       lorem_results.each do |lr|
         @site.crawling_errors.create(error_type: 'Lorem ipsum', url: url, info: lr.to_html)
       end
     end
-
-    find_links_on_page(url)
   end
 
-  def find_links_on_page(url)
+  def gather_links_on_page(url)
+    doc = Nokogiri::HTML open(url)
+    links = doc.css 'a'
+    hrefs = links.map { |link| link.attribute('href').to_s }.uniq.sort.delete_if do |href|
+      invalid_link_format href
+    end
   end
+
+  def invalid_link_format(href)
+    href.empty? || href == @site.url
+  end
+
 end
