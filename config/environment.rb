c0e186b13ccb58df665493bf796070cf8725b2b2
Tim Green <tiimgreen@gmail.com>
2014-08-04 11:51:39 +0100

    Speed up Nokogiri

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Speed up Nokogiri as mentioned here: http://rubyglasses.blogspot.co.uk/2009/07/40-speedup-using-nokogiri.html
+config.gem "nokogiri"
+ActiveSupport::XmlMini.backend='Nokogiri'
+
 # Initialize the Rails application.
 Rails.application.initialize!
