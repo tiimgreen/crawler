c4fd12268bc5c6f101dd3e26f58a698a9af40eb3
Tim Green <tiimgreen@gmail.com>
2014-08-04 10:14:17 +0100

    Add comment helper

--- app/controllers/crawls_controller.rb
+++ app/controllers/crawls_controller.rb
@@ -1,7 +1,9 @@
 class CrawlsController < ApplicationController
   def new
-    #Delayed::Job.enqueue CrawlJob.new(params[:site_id])
-    CrawlJob.new(params[:site_id]).perform
+    Delayed::Job.enqueue CrawlJob.new(params[:site_id])
+    # If you want to run the crawl not in the background, uncomment
+    # the following line:
+    # CrawlJob.new(params[:site_id]).perform
     flash[:success] = 'Crawling site'
     redirect_to site_path params[:site_id]
   end
