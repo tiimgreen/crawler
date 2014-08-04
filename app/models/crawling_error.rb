18b078d08a81256af018f8337453fe47497b1efa
Tim Green <tiimgreen@gmail.com>
2014-08-04 10:32:26 +0100

    Add validation for error fields

--- app/models/crawling_error.rb
+++ app/models/crawling_error.rb
@@ -1,3 +1,8 @@
 class CrawlingError < ActiveRecord::Base
   belongs_to :site
+
+  validates :error_type, presence: true
+  validates :url,        presence: true
+  validates :info,       presence: true
+  validates :site_id,    presence: true
 end
