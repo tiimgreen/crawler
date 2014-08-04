e768442fbe02b42ce7aea60f6642b2932ff3268a
Tim Green <tiimgreen@gmail.com>
2014-08-04 09:46:06 +0100

    Update validation

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,14 @@
 class Site < ActiveRecord::Base
+  before_save :format_link
+
   has_many :crawling_errors, dependent: :destroy
+
+  validates :name, presence: true
+  validates :url,  presence: true, format: URI::regexp(%w(http https))
+
+  private
+
+  def format_link
+    self.url = url[0, url.length - 1] if self.url.split('').last == '/'
+  end
 end
